4fce1d6ddd18c8ea842e085b8d08988802b5e8fb
David Stewart <stewart9376@hotmail.com>
2024-03-14 16:50:15 +0000

    complete pair programming

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,43 +8,104 @@ import maryam from "../../assets/class-photos/Maryam Al-Neama.jpg";
 import sarah from "../../assets/class-photos/Sarah Naunton.jpg";
 import sol from "../../assets/class-photos/Solomon Pipi.jpg";
 import thinnapat from "../../assets/class-photos/Thinnapat Prasopakarakit.jpg";
-import vlad from "../../assets/class-photos/Vlad.png";
 import halim from "../../assets/class-photos/Halim Mouaziz.jpg";
 import Cards from "../../components/Cards/Cards";
 import "./Card.scss";
+import { useEffect, useState } from "react";
 
 export default function Card() {
-  const pictures = [
-    { id: 1, img: tamara, stat: "" },
-    { id: 1, img: tamara, stat: "" },
-    { id: 2, img: andrew, stat: "" },
-    { id: 2, img: andrew, stat: "" },
-    { id: 3, img: casper, stat: "" },
-    { id: 3, img: casper, stat: "" },
-    { id: 4, img: david, stat: "" },
-    { id: 4, img: david, stat: "" },
-    { id: 5, img: kenny, stat: "" },
-    { id: 5, img: kenny, stat: "" },
-    { id: 6, img: halim, stat: "" },
-    { id: 6, img: halim, stat: "" },
-    { id: 7, img: leon, stat: "" },
-    { id: 7, img: leon, stat: "" },
-    { id: 8, img: thinnapat, stat: "" },
-    { id: 8, img: thinnapat, stat: "" },
-    { id: 9, img: sarah, stat: "" },
-    { id: 9, img: sarah, stat: "" },
-    { id: 10, img: maryam, stat: "" },
-    { id: 10, img: maryam, stat: "" },
-    { id: 11, img: sol, stat: "" },
-    { id: 11, img: sol, stat: "" },
-    { id: 12, img: vlad, stat: "" },
-    { id: 12, img: vlad, stat: "" },
-  ].sort(() => Math.random() - 0.5);
+  const picFunc = () => {
+    return [
+      { key: 1, img: tamara, isMatched: false },
+      { key: 1, img: tamara, isMatched: false },
+      { key: 2, img: andrew, isMatched: false },
+      { key: 2, img: andrew, isMatched: false },
+      { key: 3, img: casper, isMatched: false },
+      { key: 3, img: casper, isMatched: false },
+      { key: 4, img: david, isMatched: false },
+      { key: 4, img: david, isMatched: false },
+      { id: 5, img: kenny, isMatched: false },
+      { key: 5, img: kenny, isMatched: false },
+      { key: 6, img: halim, isMatched: false },
+      { key: 6, img: halim, isMatched: false },
+      { key: 7, img: leon, isMatched: false },
+      { key: 7, img: leon, isMatched: false },
+      { key: 8, img: thinnapat, isMatched: false },
+      { key: 8, img: thinnapat, isMatched: false },
+      { key: 9, img: sarah, isMatched: false },
+      { key: 9, img: sarah, isMatched: false },
+      { key: 10, img: maryam, isMatched: false },
+      { key: 10, img: maryam, isMatched: false },
+      { key: 11, img: sol, isMatched: false },
+      { key: 11, img: sol, isMatched: false },
+    ].sort(() => Math.random() - 0.5);
+  };
+
+  const [pics, setPics] = useState([]);
+  const [flippedId, setFlippedId] = useState([]);
+  const [canFlip, setCanFlip] = useState(true);
+
+  useEffect(() => {
+    setPics(picFunc());
+  }, []);
+
+  useEffect(() => {
+    const [firstId, secondId] = flippedId;
+    if (flippedId.length === 2) {
+      setCanFlip(false);
+
+      if (pics[firstId].key === pics[secondId].key) {
+        setPics((prevPics) =>
+          prevPics.map((pic, index) => ({
+            ...pic,
+            isMatched: index === firstId || index === secondId,
+          }))
+        );
+
+        setFlippedId([]);
+        setCanFlip(true);
+      } else if (
+        pics[firstId] &&
+        pics[secondId] &&
+        pics[firstId].key !== pics[secondId.key]
+      ) {
+        setTimeout(() => {
+          setFlippedId([]);
+          setCanFlip(true);
+        }, 2000);
+      }
+    }
+  }, [flippedId]);
+
+  const flipPic = (index) => {
+    if (!canFlip || flippedId.includes[index] || pics[index].isMatched) return;
+    setFlippedId([...flippedId, index]);
+  };
+
   return (
-    <div className="card">
-      {pictures.map((picture, index) => (
-        <img key={index} src={picture.img} alt="" className="card__image" />
+    <>
+      {pics.map((picture, index) => (
+        <div
+          key={index}
+          className={`card ${
+            picture.isMatched
+              ? "card--matched"
+              : flippedId.includes(index)
+              ? "card--flipped"
+              : "card--unmatched"
+          }`}
+          onClick={() => flipPic(index)}
+        >
+          <img
+            key={index}
+            src={picture.img}
+            alt=""
+            className={`card__image ${
+              picture.isMatched ? "card__image--matched" : ""
+            }`}
+          />
+        </div>
       ))}
-    </div>
+    </>
   );
 }
